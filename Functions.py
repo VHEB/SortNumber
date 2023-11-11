cb49a19752f60fc21c0f743420612d3364420eb6
The Guardian <vheb0316@gmail.com>
2023-11-10 22:06:13 -0300

    first commit

--- Functions.py
+++ Functions.py
@@ -1,5 +1,20 @@
-def Somar(x,y):
-    x = x + y
+def MaiorOrMenor(Sort_value, User_value):
+    if User_value > Sort_value:
+        result = User_value - Sort_value
+        print("Errou, o número é menor que " , User_value)
+        print("A diferença entre o número Sorteado e o seu Palpite é de: " , result + 3, " mais ou menos ;)")
+    elif User_value < Sort_value:
+        result = Sort_value - User_value
+        print("Errou, o número é maior que " , User_value)
+        print("A diferença entre o número Sorteado e o seu Palpite é de: " , result + 3, " mais ou menos ;)")
+   
+def Quadrado(Sort_value, User_value):
+    result = Sort_value * Sort_value
+    print("O número sorteado é a Raiz de " , result)
 
-def Subtracao(x,y):
-    x = x - y
+def ParOrImpar(Sort_value, User_value):
+    if Sort_value % 2 == 0:
+        print("O número Sorteado é par!")
+    else:
+        print("O número Sorteado é Impar")
+    
--- SortNumber.py
+++ SortNumber.py
@@ -5,60 +5,35 @@ import os
 import Functions
 
 #Aqui Sorteia O NÚMERO
-Sort_Num = random.randint(0,10)
-Burn = [2,3,4,5]
-Sort_Burn = random.choice(Burn)
+Sort_Num = random.randint(0,100)
+Try_User = 0
 User_Num = 0
 
 #Texto Introdutorio
 print("Olá, meu nome é Vitor Hugo \n Esse é meu primeiro projeto em Pyhton \n SORT NUMBER \n")
 print("Seu objetivo caro jogador é descobrir o Número que foi gerado aleatoriamente. \n Boa Sorte \n")
 
-#Vamos para a lógica
 
-#Aqui o Usúario adiciona um Número
-User_Num = int(input("Digite um Número: "))
-
-if Sort_Num != User_Num:
-    while Sort_Num != User_Num:    
-        if Sort_Num < User_Num:
-            if Functions.Somar(User_Num, Sort_Burn) == Sort_Num:
-                os.system('cls')
-                print("Está Quente! \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
-            elif Functions.Somar(User_Num, Sort_Burn*2) == Sort_Num:
-                os.system('cls')
-                print("Está Morno! \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
-            elif Functions.Somar(User_Num, Sort_Burn*3) == Sort_Num:
-                os.system('cls')
-                print("Está Frio! \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
+while User_Num is not Sort_Num:
+    
+    Try_User += 1
+   
+    try:
+        User_Num = int(input("Adivinhe o Número: "))
+        os.system('cls')
+        if User_Num == Sort_Num:
+            print("Meus Parabês você Acertou! O número era " , Sort_Num , " e levou apenas " , Try_User , " tentativas.")
+        else:
+            i = random.randint(0,2)
+            if i == 0:
+                Functions.MaiorOrMenor(Sort_Num,User_Num)
+            elif i == 1:
+                Functions.ParOrImpar(Sort_Num,User_Num)
             else:
-                os.system('cls')
-                print("Glacial \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
+                Functions.Quadrado(Sort_Num,User_Num) 
+    except ValueError:
+        print("Valor Digitado invalido.")
 
-        elif Sort_Num > User_Num:
-            if Functions.Subtracao(User_Num, Sort_Burn) == Sort_Num:
-                os.system('cls')
-                print("Está Quente! \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
-            elif Functions.Subtracao(User_Num, Sort_Burn*2) == Sort_Num:
-                os.system('cls')
-                print("Está Morno! \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
-            elif Functions.Subtracao(User_Num, Sort_Burn*3) == Sort_Num:
-                os.system('cls')
-                print("Está Frio! \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
-            else:
-                os.system('cls')
-                print("Glacial \n")
-                User_Num = int(input("Digite um Número Novamente: \n"))
-                
-elif Sort_Num == User_Num:
-    print("Meus Parbêns você conseguiu!")
-else:
-    print("Por Favor Digite um Número Inteiro.")
+
+
 
